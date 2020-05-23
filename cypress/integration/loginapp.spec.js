cb357f1cf1020ee48e8d382409d224d31af4aab6
milecki <m.kulecki@gmail.com>
2020-05-23 16:35:56 +0200

    test: requiring password to log in

--- cypress/integration/loginapp.spec.js
+++ cypress/integration/loginapp.spec.js
@@ -5,11 +5,19 @@ describe('UI TESTS', () => {
     cy.get('[data-cy="login-text"]').should('be.visible');
   });
 
-  it('should should not login when username is not provided', () => {
+  it('should not login when username is not provided', () => {
     cy.visit('http://localhost:3000');
-    cy.get('#inputPassword').type('123456');
+    cy.get('[data-cy=password]').type('123456');
     cy.get('[data-cy=submit-button]').click();
     // check that we are still on the same page
+    cy.get('[data-cy="login-text"]').should('have.length', 1);
+    cy.get('[data-cy="homepage"]').should('have.length', 0);
+  });
+
+  it('should not login when password is not provided', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-cy=email]').type('john@example.com');
+    cy.get('[data-cy=submit-button]').click();
     cy.get('[data-cy="login-text"]').should('have.length', 1);
     cy.get('[data-cy="homepage"]').should('have.length', 0);
   });
