f56e2abae0dadbc169c1b0c1cba995230bddfa4e
milecki <m.kulecki@gmail.com>
2020-05-23 16:20:31 +0200

    tests page loading & requiring username to log in

--- cypress/integration/loginapp.spec.js
+++ cypress/integration/loginapp.spec.js
@@ -1,5 +1,16 @@
 describe('UI TESTS', () => {
-  it("should navigate to Google's website", () => {
-    cy.visit('www.google.com');
+  it('should load the login page correctly', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('[data-cy="login-text"]').should('have.length', 1);
+    cy.get('[data-cy="login-text"]').should('be.visible');
+  });
+
+  it('should should not login when username is not provided', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('#inputPassword').type('123456');
+    cy.get('[data-cy=submit-button]').click();
+    // check that we are still on the same page
+    cy.get('[data-cy="login-text"]').should('have.length', 1);
+    cy.get('[data-cy="homepage"]').should('have.length', 0);
   });
 });
